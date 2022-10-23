d978eed5e79467f9fca33b21085ae67c3b166cd4
AkromSaidxonov <101202313+AkromSaidxonov@users.noreply.github.com>
2022-10-23 18:55:50 +0500

    last

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home";
 import Add from "./components/Add";
-import Edit from "./components/Edit";
+
 function App() {
   return (
     <div>
       <Add />
-      {/* <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/Add" element={<Add />} />
-        <Route path="/Edit/:postID" element={<Edit />} />
-      </Routes> */}
+
     </div>
   );
 }
